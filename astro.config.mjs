abc3d4e30cf93f3016ed2175ec857a9207e8e944
Michał Dziuba <mail@michaldziuba.dev>
2024-11-11 00:17:12 +0100

    feat: customize docs theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,47 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+  integrations: [
+    starlight({
+      title: "PandJS",
+      credits: true,
+      description: "PandJS is a free and open-source runtime.",
+      lastUpdated: true,
+      titleDelimiter: "-",
+      logo: {
+        dark: "./src/assets/pandlogo-dark.svg",
+        light: "./src/assets/pandlogo-light.svg",
+        alt: "PandJS logo",
+        replacesTitle: true,
+      },
+      social: {
+        github: "https://github.com/pandland/pand",
+      },
+      editLink: {
+        baseUrl: "https://github.com/pandland/pandland.github.io/edit/v2/",
+      },
+      sidebar: [
+        {
+          label: "Learn",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: [
+        "@fontsource/prompt/400.css",
+        "@fontsource/prompt/600.css",
+				"./src/tailwind.css",
 			],
-		}),
-	],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
